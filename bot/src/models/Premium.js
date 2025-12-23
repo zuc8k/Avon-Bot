37af1775443cf814d6c2331c1a368dbe16a832c4
Boody Zuckerberg <abdohassan9090@icloud.com>
2025-12-23 16:11:11 +0300

    Update Premium.js

--- bot/src/models/Premium.js
+++ bot/src/models/Premium.js
@@ -1,17 +1,17 @@
 const mongoose = require('mongoose');
 
-module.exports = mongoose.model(
-  'Premium',
-  new mongoose.Schema({
-    userId: String,
-    guildId: String,
-    plan: {
-      type: String,
-      enum: ['free', 'plus', 'premium', 'max'],
-      default: 'free'
-    },
-    startAt: Date,
-    endAt: Date,
-    active: Boolean
-  })
-);
+const premiumSchema = new mongoose.Schema({
+  userId: String,
+  guildId: String,
+  plan: {
+    type: String,
+    enum: ['free', 'plus', 'premium', 'max'],
+    default: 'free'
+  },
+  active: { type: Boolean, default: true },
+  expiresAt: Date
+});
+
+premiumSchema.index({ userId: 1, guildId: 1 }, { unique: true });
+
+module.exports = mongoose.model('Premium', premiumSchema);
