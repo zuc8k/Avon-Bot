891d9f437702c140aebeab07766e6564bc9463da
Boody Zuckerberg <abdohassan9090@icloud.com>
2025-12-23 16:05:37 +0300

    Update Wallet.js

--- bot/src/models/Wallet.js
+++ bot/src/models/Wallet.js
@@ -1,9 +1,11 @@
 const mongoose = require('mongoose');
 
 const walletSchema = new mongoose.Schema({
-  userId: String,
-  guildId: String,
+  userId: { type: String, required: true },
+  guildId: { type: String, required: true },
   balance: { type: Number, default: 0 }
 });
+
+walletSchema.index({ userId: 1, guildId: 1 }, { unique: true });
 
 module.exports = mongoose.model('Wallet', walletSchema);
