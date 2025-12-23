fb2de8e9fd528130a332a83780d58b0b8344fb81
Boody Zuckerberg <abdohassan9090@icloud.com>
2025-12-23 15:50:37 +0300

    Update index.js

--- bot/src/index.js
+++ bot/src/index.js
@@ -1,18 +1,32 @@
 require('dotenv').config();
-const { Client, GatewayIntentBits } = require('discord.js');
+const { Client, GatewayIntentBits, Events } = require('discord.js');
 const connectMongo = require('./config/mongo');
+const registerCommands = require('./handlers/commands');
 
 const client = new Client({
   intents: [
-    GatewayIntentBits.Guilds,
-    GatewayIntentBits.GuildMessages,
-    GatewayIntentBits.MessageContent
+    GatewayIntentBits.Guilds
   ]
 });
 
-client.once('ready', () => {
+client.once(Events.ClientReady, async () => {
   console.log(`🤖 AVON BOT logged in as ${client.user.tag}`);
+  await registerCommands(client);
 });
 
-client.login(process.env.BOT_TOKEN);
+client.on(Events.InteractionCreate, async interaction => {
+  if (!interaction.isChatInputCommand()) return;
+
+  const command = client.commands.get(interaction.commandName);
+  if (!command) return;
+
+  try {
+    await command.execute(interaction);
+  } catch (err) {
+    console.error(err);
+    interaction.reply({ content: '❌ Error executing command', ephemeral: true });
+  }
+});
+
 connectMongo();
+client.login(process.env.BOT_TOKEN);
